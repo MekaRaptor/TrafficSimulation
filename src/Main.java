a4f3c76e1afafd6847845ba6b86635dc4e4c8099
MekaRaptor <106775708+MekaRaptor@users.noreply.github.com>
2025-04-23 17:53:06 +0300

    Road and Vehicle Integration

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-// src/Main.java
+import core.Road;
 import core.Vehicle;
 
 public class Main {
     public static void main(String[] args) {
-        Vehicle car1 = new Vehicle("A1");
-        Vehicle car2 = new Vehicle("B2");
+        Road road1 = new Road("R1", 1);  // Maksimum 1 araç
+        Vehicle car1 = new Vehicle("A1", road1);
+        Vehicle car2 = new Vehicle("B2", road1);
 
         car1.start();
         car2.start();
--- src/core/Vehicle.java
+++ src/core/Vehicle.java
@@ -1,13 +1,14 @@
-// src/core/Vehicle.java
 package core;
 
 public class Vehicle extends Thread {
     private final String id;
     private int position;
     private boolean active;
+    private final Road road;
 
-    public Vehicle(String id) {
+    public Vehicle(String id, Road road) {
         this.id = id;
+        this.road = road;
         this.position = 0;
         this.active = true;
     }
@@ -15,8 +16,15 @@ public class Vehicle extends Thread {
     @Override
     public void run() {
         while (active) {
-            move();
             try {
+                // Yol müsaitse hareket et, değilse bekle
+                if (road.addVehicle(this)) {
+                    move();
+                    road.removeVehicle(this); // hareketten sonra yoldan çık
+                } else {
+                    System.out.println("Vehicle " + id + " is waiting: road " + road.getId() + " is full.");
+                }
+
                 Thread.sleep(200);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
@@ -26,7 +34,7 @@ public class Vehicle extends Thread {
 
     private void move() {
         position++;
-        System.out.println("Vehicle " + id + " moved to position " + position);
+        System.out.println("Vehicle " + id + " moved to position " + position + " on road " + road.getId());
     }
 
     public String getVehicleId() {
